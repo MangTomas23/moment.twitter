612923eca9dd4a15c4b3ef7033be3f2426020bb5
Adrian Paul Matos <matos.adrianpaul@gmail.com>
2019-01-22 16:25:31 +0800

    Display full date if greater than 1 year

--- moment-twitter.js
+++ moment-twitter.js
@@ -1,5 +1,5 @@
 (function() {
-  var day, formats, hour, initialize, minute, second, week;
+  var day, formats, hour, initialize, minute, second, week, month, year;
 
   second = 1e3;
 
@@ -11,22 +11,26 @@
 
   week = 6048e5;
 
+  month = 24192e5;
+
+  year = 290304e5;
+
   formats = {
     seconds: {
-      short: 's',
-      long: ' sec'
+      short: "s",
+      long: " sec"
     },
     minutes: {
-      short: 'm',
-      long: ' min'
+      short: "m",
+      long: " min"
     },
     hours: {
-      short: 'h',
-      long: ' hr'
+      short: "h",
+      long: " hr"
     },
     days: {
-      short: 'd',
-      long: ' day'
+      short: "d",
+      long: " day"
     }
   };
 
@@ -38,49 +42,50 @@
       unit = null;
       num = null;
       if (diff <= second) {
-        unit = 'seconds';
+        unit = "seconds";
         num = 1;
       } else if (diff < minute) {
-        unit = 'seconds';
+        unit = "seconds";
       } else if (diff < hour) {
-        unit = 'minutes';
+        unit = "minutes";
       } else if (diff < day) {
-        unit = 'hours';
-      } else if (format === 'short') {
+        unit = "hours";
+      } else if (format === "short") {
         if (diff < week) {
-          unit = 'days';
+          unit = "days";
         } else {
-          return this.format('M/D/YY');
+          return this.format("M/D/YY");
         }
+      } else if (diff < year) {
+        return this.format("MMM D");
       } else {
-        return this.format('MMM D');
+        return this.format("MMM D, YYYY");
       }
       if (!(num && unit)) {
         num = moment.duration(diff)[unit]();
       }
       unitStr = unit = formats[unit][format];
-      if (format === 'long' && num > 1) {
-        unitStr += 's';
+      if (format === "long" && num > 1) {
+        unitStr += "s";
       }
       return num + unitStr;
     };
     moment.fn.twitterLong = function() {
-      return twitterFormat.call(this, 'long');
+      return twitterFormat.call(this, "long");
     };
     moment.fn.twitter = moment.fn.twitterShort = function() {
-      return twitterFormat.call(this, 'short');
+      return twitterFormat.call(this, "short");
     };
     return moment;
   };
 
-  if (typeof define === 'function' && define.amd) {
-    define('moment-twitter', ['moment'], function(moment) {
-      return this.moment = initialize(moment);
+  if (typeof define === "function" && define.amd) {
+    define("moment-twitter", ["moment"], function(moment) {
+      return (this.moment = initialize(moment));
     });
-  } else if (typeof module !== 'undefined') {
-    module.exports = initialize(require('moment'));
+  } else if (typeof module !== "undefined") {
+    module.exports = initialize(require("moment"));
   } else if (typeof window !== "undefined" && window.moment) {
     this.moment = initialize(this.moment);
   }
-
-}).call(this);
+}.call(this));
